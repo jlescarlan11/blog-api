fd77ddbc270f5ba567279a59c23809ef4f3e6bc0
john lester escarlan <jlescarlan11@gmail.com>
2025-05-10 22:41:19 +0800

    added nav log o in admin

--- frontend-admin/src/components/Navbar.tsx
+++ frontend-admin/src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../auth";
+import { LuNewspaper } from "react-icons/lu";
 
 interface NavbarProps {
   children?: React.ReactNode;
@@ -17,7 +18,10 @@ const Navbar: React.FC<NavbarProps> = ({ children }) => {
 
   return (
     <div className="flex items-center justify-between p-4 bg-base-100 shadow">
-      <div className="text-xl font-semibold">Navbar</div>
+      <div className="text-xl flex items-center gap-4">
+        <LuNewspaper className="" />
+        <h1 className="font-bold">Sutta's Blog</h1>
+      </div>
       <div className="flex items-center space-x-4">
         {children}
         <button onClick={handleLogout} className="btn btn-sm btn-outline">
